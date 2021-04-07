23be39f9b7cca09a2bb4d80555335fad071fb1da
Erich Barnstedt <erichb@microsoft.com>
2021-04-07 20:18:15 +0200

    Added initial sample for AASX file in Open Packaging Conventions (OPC) format.

--- StationNodeManager.cs
+++ StationNodeManager.cs
@@ -4,6 +4,8 @@ using Opc.Ua.Server;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Packaging;
+using System.Net.Mime;
 using System.Threading;
 
 namespace Opc.Ua.Sample
@@ -121,8 +123,53 @@ namespace Opc.Ua.Sample
 
         private ServiceResult OnGenerateAASCall(ISystemContext context, MethodState method, IList<object> inputArguments, IList<object> outputArguments)
         {
-            // TODO
+            // Convert system path and file names to Part URIs.
+            Uri partUriDocument = PackUriHelper.CreatePartUri(new Uri("Content\\Document.xml", UriKind.Relative));
+            Uri partUriResource = PackUriHelper.CreatePartUri(new Uri("Resources\\Image1.jpg", UriKind.Relative));
+
+            string documentPath = string.Empty;
+            string resourcePath = string.Empty;
+            string packagePath = string.Empty;
+
+            using (Package package = Package.Open(packagePath, FileMode.Create))
+            {
+                // Add the Document part to the Package
+                PackagePart packagePartDocument = package.CreatePart(partUriDocument, System.Net.Mime.MediaTypeNames.Text.Xml);
+
+                // Copy the data to the Document Part
+                using (FileStream fileStream = new FileStream(documentPath, FileMode.Open, FileAccess.Read))
+                {
+                    CopyStream(fileStream, packagePartDocument.GetStream());
+                }
+
+                // Add a Package Relationship to the Document Part
+                package.CreateRelationship(packagePartDocument.Uri, TargetMode.Internal, "PackageRelationshipType");
+
+                // Add a Resource Part to the Package
+                PackagePart packagePartResource = package.CreatePart(partUriResource, MediaTypeNames.Image.Jpeg);
+
+                // Copy the data to the Resource Part
+                using (FileStream fileStream = new FileStream(resourcePath, FileMode.Open, FileAccess.Read))
+                {
+                    CopyStream(fileStream, packagePartResource.GetStream());
+                }
+
+                // Add Relationship from the Document part to the Resource part
+                packagePartDocument.CreateRelationship(new Uri(@"../resources/image1.jpg", UriKind.Relative), TargetMode.Internal, "ResourceRelationshipType");
+            }
+              
             return ServiceResult.Good;
+        }
+
+        private void CopyStream(Stream source, Stream target)
+        {
+            const int bufSize = 0x1000;
+            byte[] buf = new byte[bufSize];
+            int bytesRead = 0;
+            while ((bytesRead = source.Read(buf, 0, bufSize)) > 0)
+            {
+                target.Write(buf, 0, bytesRead);
+            }
         }
 
         private void ImportNodeset2Xml(IDictionary<NodeId, IList<IReference>> externalReferences, string resourcepath)
